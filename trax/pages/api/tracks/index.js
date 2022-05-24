75aac3e609adc0b6cd6eeb2f66e10eb277e31bba
Richard Immergluck <rimmergluck@googlemail.com>
2022-05-24 14:26:51 +0100

    added track to schema and working api call

--- trax/pages/api/tracks/index.js
+++ trax/pages/api/tracks/index.js
@@ -1,3 +1,11 @@
-// export default function handler(req, res) {
-//   res.status(200).json('tracks')
-// }
+
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+const trackList = await prisma.track.findMany();
+res.json(trackList)
+}
+
