bca2fa10e9ad8f5bf6d7bb7ed3f42fc463ea398b
Richard Immergluck <rimmergluck@googlemail.com>
2022-05-26 10:20:43 +0100

    Create global prisma component

--- trax/pages/api/tracks/index.js
+++ trax/pages/api/tracks/index.js
@@ -1,7 +1,5 @@
 import { getSession } from 'next-auth/react'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '/components/prisma'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
@@ -11,23 +9,23 @@ export default async function handler(req, res) {
 
   if (req.method === 'POST') {
     try {
-      const trackData = JSON.stringify(req.body)
 
+      // Destructure the req.body
       const { title, composer } = req.body
 
+      // Use getSession Hook to access current user
       const session = await getSession({ req })
 
+      // Create DB entry that will be uploaded
       const upTrack = await prisma.track.create({
         data: {
           title: title,
           composer: composer,
-          uploadedBy: { connect: { email: session?.user?.email } }
+          uploadedBy: { connect: { email: session?.user?.email } } 
         }
       })
 
-      res.status(200).json(upTrack)
-
-      // pull the id of the most recently created track
+      // pull the id of the just created track
       const newTrackId = await prisma.track.findMany({
         where: {
           title: title,
@@ -43,6 +41,8 @@ export default async function handler(req, res) {
       })
 
       console.log(newTrackId)
+
+      res.status(200).json(upTrack, newTrackId)
       
     } catch (err) {
       console.log('from API error', err)
