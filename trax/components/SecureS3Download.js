6fd1e56e489b2f114ce64d0cb4a774a20f711493
Richard Immergluck <rimmergluck@googlemail.com>
2022-06-27 20:17:54 +0100

    download working with signed urls in catalogue

--- trax/components/SecureS3Download.js
+++
@@ -1,6 +0,0 @@
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-
-const client = new S3Client(clientParams);
-const command = new GetObjectCommand(getObjectParams);
-const url = await getSignedUrl(client, command, { expiresIn: 3600 });
