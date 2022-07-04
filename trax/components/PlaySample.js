0b4375f6e8c57b815296d8e478cc6e1611f0a49e
Richard Immergluck <rimmergluck@googlemail.com>
2022-07-04 09:34:42 +0100

    preview audio needs new approach

--- trax/components/PlaySample.js
+++ trax/components/PlaySample.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import GetSignedS3URL from './GetSignedS3URL'
 
-const PlaySample = (props) => {
-
+const PlaySample = props => {
   const { track, start, stop } = props
+
   //Create presigned URL
   const url = GetSignedS3URL({
     bucket: 'backingtrackstorage',
     key: `${track.fileName}`,
-    expires: 600,
+    expires: 600
   })
 
-  // concat presignedurl with playback range
-  const revisedURL=`${url}#t=${start},${stop}`
-  console.log(revisedURL)
+  // Concat presignedurl with playback range
+  const revisedURL = `${url}#t=${start},${stop}`
 
+  // Render the JSX
   return (
-    <div>
-      <audio controls preload='auto'>
+    <div key={track.id}>
+      <audio controls preload='metadata'>
         <source src={revisedURL} type='audio/mp3' />
+        <source src={revisedURL} type='audio/wav' />
+        <p>Your browser does not support this file type.</p>
       </audio>
     </div>
   )
