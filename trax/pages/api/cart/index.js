0566199af6f7e82b9b926d3e10e0861f2395f4a3
Richard Immergluck <rimmergluck@googlemail.com>
2022-07-07 17:02:40 +0100

    Added DB models and fixed adding to cart

--- trax/pages/api/cart/index.js
+++ trax/pages/api/cart/index.js
@@ -1,9 +1,10 @@
 import { getSession } from 'next-auth/react'
+
 import prisma from '/components/prisma'
 
 export default async function handler(req, res) {
   
-  // Not POST error
+  // Fetch All TRacks
   if (req.method !== 'POST') {
     res.status(500).json({ message: 'Something went wrong' })
   }
@@ -11,8 +12,9 @@ export default async function handler(req, res) {
   // Upload a single track to DB
   if (req.method === 'POST') {
     try {
+
       // Destructure the req.body
-      const { newFileName, title, composer, previewStart, previewEnd, price, formattedPrice } = req.body
+      const {} = req.body
       // console.log('req body is', req.body)
 
       // Use getSession Hook to access current user
@@ -31,8 +33,6 @@ export default async function handler(req, res) {
           formattedPrice: formattedPrice
         }
       })
-
-      console.log('the new track filename is ====>  ', newFileName)
 
       res.status(200).json(upTrack, newFileName)
 
--- trax/pages/cart.js
+++ trax/pages/cart.js
@@ -1,13 +1,50 @@
+import React from 'react'
 import { useCart } from 'react-use-cart'
 import Link from 'next/link'
+import prisma from '/components/prisma'
 
-function Cart() {
+// Retrieve all the tracks from the DB
+export const getStaticProps = async () => {
+  const tracks = await prisma.track.findMany({
+    select: {
+      id: true,
+      price: true,
+    }
+  })
+
+  return {
+    props: {
+      tracks
+    }
+  }
+}
+
+function Cart({ tracks }) {
+  // useCart hook
   const { emptyCart, removeItem, cartTotal, items } = useCart()
 
-  const buy = () => {
-    alert("You've just bought some tracks!"), emptyCart()
+  var formatter = new Intl.NumberFormat('en-UK', {
+    style: 'currency',
+    currency: 'GBP',
+  });
+
+  var total=formatter.format(cartTotal)
+
+  const checkout = () => {
+    alert("You've just bought some tracks!")
+
+    // Check cart values against DB
+    for (var arrayObject = 0; arrayObject < items.length; arrayObject++) {
+      console.log(items[arrayObject].id)
+      console.log(tracks[arrayObject].price)
+    }
+
+    console.log(tracks)
+    // Create DB Submission Object
+    const submissionData = {}
+    // Create some fake data to send to the API
   }
-  
+
   return (
     <div className='ms-2'>
       <br />
@@ -22,13 +59,13 @@ function Cart() {
             </Link>
           </div>
           <div>{item.composer}</div>
-          <div>{item.price}</div>
+          <div>{item.formattedPrice}</div>
           <button onClick={() => removeItem(item.id)}>Remove Item</button>
           <hr />
         </div>
       ))}
-      <p>Total = {cartTotal}</p>
-      <button onClick={buy} className='btn btn-primary ms-2'>
+      <p>Total = {total}</p>
+      <button onClick={checkout} className='btn btn-primary ms-2'>
         Buy Now
       </button>
     </div>
