ba92af3eacc40e29dce499570181ec46f0db4999
Richard Immergluck <rimmergluck@googlemail.com>
2022-09-07 11:12:56 +0100

    removal of unused files

--- trax/pages/api/checkout_sessions.js
+++ trax/pages/api/checkout_sessions.js
@@ -1,3 +1,5 @@
+// reference https://stripe.com/docs/api/checkout/sessions/create
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
--- trax/pages/api/download.js
+++ trax/pages/api/download.js
@@ -1,4 +1,3 @@
-import { DeleteBucketOwnershipControlsCommand } from '@aws-sdk/client-s3'
 import AWS from 'aws-sdk'
 
 AWS.config.update({
