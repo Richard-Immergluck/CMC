847ea051ff59edb9e6bc7163c808c1bdf0156ea8
Richard-Immergluck <97466331+Richard-Immergluck@users.noreply.github.com>
2022-12-18 19:03:17 +0000

    Over-complicated prisma client generator FIXED!

--- trax/components/prisma.js
+++ trax/components/prisma.js
@@ -1,14 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-  console.log('production prisma client created')
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-  console.log('development prisma client created')
-}
+// My overcomplicated attempt to get prisma to work in dev and prod
+// let prisma;
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+//   console.log('production prisma client created')
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+//   console.log('development prisma client created')
+// }
+
+const prisma =  new PrismaClient();
 
 export default prisma;
