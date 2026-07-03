c5c39d65e41697e323622cba83a94c65dde4a3b3
Richard Immerglück <richardimmergluck@Jellyfishs-MacBook-Pro.local>
2026-07-03 16:20:55 +0100

    Fix cookie banner script warning

--- components/CookieConsentBanner.js
+++ components/CookieConsentBanner.js
@@ -1,12 +1,7 @@
 import { cookies } from 'next/headers'
-import Link from 'next/link'
+import CookieConsentBannerContent from './CookieConsentBannerContent'
 
 const CONSENT_COOKIE = 'cmc_cookie_consent'
-const dismissScript = `
-  document.getElementById('cmc-cookie-consent-form')?.addEventListener('submit', function () {
-    document.querySelector('.cmc-cookie-banner')?.setAttribute('hidden', '');
-  });
-`
 
 const CookieConsentBanner = async () => {
   const cookieStore = await cookies()
@@ -16,27 +11,7 @@ const CookieConsentBanner = async () => {
     return null
   }
 
-  return (
-    <aside className='cmc-cookie-banner' aria-label='Cookie notice'>
-      <div>
-        <p className='cmc-cookie-banner__title'>Cookies on Classical Music Catalogue</p>
-        <p>
-          We currently use necessary cookies for sign-in, security, and payments.
-          Optional analytics or marketing cookies are not enabled.
-        </p>
-      </div>
-      <form
-        action='/api/cookie-consent'
-        className='cmc-cookie-banner__actions'
-        id='cmc-cookie-consent-form'
-        method='post'
-      >
-        <Link href='/cookies'>Cookie policy</Link>
-        <button type='submit'>Accept necessary cookies</button>
-      </form>
-      <script dangerouslySetInnerHTML={{ __html: dismissScript }} />
-    </aside>
-  )
+  return <CookieConsentBannerContent />
 }
 
 export default CookieConsentBanner
